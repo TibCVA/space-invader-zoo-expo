a6a8701608a0bcfe4bd34181d0241dea0e35f5a2
TibCVA <thibaut.lacour@cva-france.fr>
2025-08-15 00:35:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -487,4 +487,4 @@ window.addEventListener('resize', ()=>{
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.aspect = window.innerWidth/window.innerHeight;
   camera.updateProjectionMatrix();
-});
+}); 
